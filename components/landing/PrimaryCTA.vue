c09c5bf6dd4516170dc28f4c3fa0276c04c063f6
SamuelJAmos <79687637+SamuelJAmos@users.noreply.github.com>
2024-10-04 14:02:37 -0500

    Added styling to buttons (follows the heading just more bold)

--- components/landing/PrimaryCTA.vue
+++ components/landing/PrimaryCTA.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2>Behind the Simplicity: My Development Journey</h2>
-    <p>Discover the techniques that make web development approachable.</p>
-    <button @click="">See the Process</button>
+    <!-- <h2 class="font-heading">Behind the Simplicity: My Development Journey</h2>
+    <p class="font-body">Discover the techniques that make web development approachable.</p> -->
+    <button @click="" class="font-heading font-bold">See the Process</button>
 </template>
--- components/landing/SecondaryCTA.vue
+++ components/landing/SecondaryCTA.vue
@@ -1,5 +1,5 @@
 <template>
-    <h2>Ready to Innovate Together?</h2>
-    <p>Let's collaborate to turn your ideas into simple, impactful solutions. Reach out, and let's start innovating.</p>
-    <button @click="">Share Your Vision</button>
+    <!-- <h2 class="font-heading">Ready to Innovate Together?</h2>
+    <p class="font-body">Let's collaborate to turn your ideas into simple, impactful solutions. Reach out, and let's start innovating.</p> -->
+    <button @click="" class="font-heading font-bold">Share Your Vision</button>
 </template>
