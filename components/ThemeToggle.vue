4c9b440a33f12d5e8630f3ec730af5208d50d248
SamuelJAmos <79687637+SamuelJAmos@users.noreply.github.com>
2024-10-06 21:40:51 -0500

    bug fixes

--- components/ThemeToggle.vue
+++ components/ThemeToggle.vue
@@ -1,15 +1,34 @@
 <template>
+  <div v-if="colorModeReady">
     <button @click="toggleColorMode">
-      {{ isDark ? 'Switch to Light Mode' : 'Switch to Dark Mode' }}
+      <Icon
+        :name="iconName"
+        size="24"
+      />
+      <!-- <span>{{ isDark ? "Switch to Light Mode" : "Switch to Dark Mode" }}</span> For Screen readers? -->
     </button>
-  </template>
-  
-  <script setup> 
-  const colorMode = useColorMode()
-  const isDark = computed(() => colorMode.value === 'dark')
-  
-  function toggleColorMode() {
-    colorMode.value = isDark.value ? 'light' : 'dark'
-  }
-  </script>
-  
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+const colorModeReady = ref(false);
+
+// Computed property to determine if dark mode is active
+const isDark = computed(() => colorMode.value === "dark");
+
+// Computed property for icon name based on color mode
+const iconName = computed(() =>
+  isDark.value ? "mdi-weather-night" : "mdi-weather-sunny"
+);
+
+// Function to toggle color mode
+function toggleColorMode() {
+  colorMode.preference = isDark.value ? "light" : "dark";
+}
+
+// Set colorModeReady once the component is mounted
+onMounted(() => {
+  colorModeReady.value = true;
+});
+</script>
