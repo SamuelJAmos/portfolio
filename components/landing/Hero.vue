c219ed501322d40c67d5a468fb9822807de597d2
SamuelJAmos <79687637+SamuelJAmos@users.noreply.github.com>
2024-10-07 19:03:13 -0500

    markdown linter

--- components/landing/Hero.vue
+++ components/landing/Hero.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
-    <h1 class="font-heading text-4xl">
+    <h1 class="font-heading text-4xl text-light-primary dark:text-dark-primary">
       Where Innovation Meets Simplicity
     </h1>
-    <p class="font-body text-base">
+    <!-- <p class="font-body text-base">
       I'm Samuel Amos, a full-stack developer dedicated to transforming complex
       challenges into simple, innovative solutions. By embracing mindful design
       practices like Object-Oriented UX and ensuring code reliability through
@@ -11,6 +11,6 @@
       people—making the digital world accessible to all.
     </p>
     <LandingPrimaryCTA />
-    <LandingSecondaryCTA />
+    <LandingSecondaryCTA /> -->
   </section>
 </template>
